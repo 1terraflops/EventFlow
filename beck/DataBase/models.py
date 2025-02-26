d72cfd744c8f7dc9afa215283c4be48b4a5dad62
Пользователь <kondrenkovlad1@gmail.com>
2025-02-26 19:49:34 +0200

    Data Base new version

--- beck/DataBase/models.py
+++ beck/DataBase/models.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.asyncio import AsyncAttrs, async_sessionmaker, create_async_
 from sqlalchemy import Integer, String, ForeignKey, LargeBinary, DateTime, func
 import asyncio
 
-database_url = 'mysql+aiomysql://ivent_db.db'
+database_url = 'mysql+aiomysql://user:password@localhost/ivent_db'
 engine = create_async_engine(database_url, echo=True)
 
 class Base (AsyncAttrs, DeclarativeBase):
@@ -16,28 +16,27 @@ class User(Base):
     password: Mapped[str] = mapped_column(String(100))
     first_name: Mapped[str] = mapped_column(String(30))
     last_name: Mapped[str] = mapped_column(String(30))
-    age = mapped_column(Integer)
+    age: Mapped[int] = mapped_column(Integer)
     role: Mapped[str] = mapped_column(String(30))
 
 class Event(Base):
     __tablename__ ='event'
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
-    time: Mapped[str] = mapped_column(String(20))
+    time: Mapped[DateTime] = mapped_column(DateTime)
     name: Mapped[str] = mapped_column(String(50))
     description: Mapped[str] = mapped_column(String(1000))
-    place = mapped_column(Integer)
+    place: Mapped[str] = mapped_column(String(10))
     location: Mapped[str] = mapped_column(String(200))
-    organizer: Mapped[str] = mapped_column(ForeignKey('users.id'))
+    organizer: Mapped[int] = mapped_column(ForeignKey('users.id'))
     image_data: Mapped[bytes] = mapped_column(LargeBinary, nullable=False)
     create_time: Mapped[DateTime] = mapped_column(DateTime, default=func.now())
     role: Mapped[str] = mapped_column(String(30))
 
-class Take_ivent(Base):
+class Take_event(Base):
     __tablename__ = 'take_event'
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
     id_event: Mapped[int] = mapped_column(ForeignKey('event.id'))
     id_user: Mapped[int] = mapped_column(ForeignKey('users.id'))
-    inf_person: Mapped[str] = mapped_column(String(150))
 
 async_session= async_sessionmaker(bind=engine, expire_on_commit=False)
 async def create_tables():
--- beck/DataBase/request.py
+++ beck/DataBase/request.py
@@ -1,6 +1,6 @@
 from DataBase.models import async_session
-from DataBase.models import User, Event, Take_ivent
-from sqlalchemy import select, update, delete, func
+from DataBase.models import User, Event, Take_event
+from sqlalchemy import select, delete, func
 from passlib.hash import bcrypt
 from email_validator import validate_email, EmailNotValidError
 from datetime import datetime
@@ -18,15 +18,14 @@ async def record_user(login, password, first_name, last_name, age, role):
             if not isinstance(age, int) or age <0:
                 return "Невірний формат віку"
             hashed_pass = bcrypt.hash(password)
-            new_user = User(
+            session.add(User(
                 login=normalized_email,
                 password=hashed_pass,
                 first_name=first_name.strip(),
                 last_name=last_name.strip(),
                 age=age,
                 role=role
-                )
-            session.add(new_user)
+                ))
             await session.commit()
             return f"Користувач з логіном {normalized_email} був доданий в бд"
     except EmailNotValidError:
@@ -35,7 +34,7 @@ async def record_user(login, password, first_name, last_name, age, role):
         return f"Помилка \n{a}"
         
 # Запис івенту
-async def record_event (id_user, name, description, location, time, image_data, role, plase):
+async def record_event (id_user, name, description, location, time, image_data, role, place):
     async with async_session() as session:
         try:
             if not all([name.strip(), description.strip(), location.strip()]):
@@ -45,17 +44,16 @@ async def record_event (id_user, name, description, location, time, image_data, 
             result = await session.scalar(select(User).where(User.id==id_user))
             if not result:
                 return "Користувача не знайдено в базі"
-            new_event = Event(
+            session.add( Event(
                 name=name.strip(),
                 description=description.strip(),
-                plase=plase,
+                place=place,
                 location=location.strip(),
                 time=time,
                 organizer=result.id,
                 image_data=image_data,
                 role=role
-            )
-            session.add(new_event)
+            ))
             await session.commit()
             return "Івент записаний"
         except SQLAlchemyError as e:
@@ -76,23 +74,119 @@ async def subscribe_event (id_user, id_ivent):
                 return False, "Івент не знайдено"
             if event.time < datetime.now():
                 return False, "Івент вже завершився"
-            excisting_sub = await session.scalar(select(Take_ivent).where(
-                Take_ivent.id_user==id_user,
-                Take_ivent.id_event==id_ivent
+            excisting_sub = await session.scalar(select(Take_event).where(
+                Take_event.id_user==id_user,
+                Take_event.id_event==id_ivent
             ))
             if excisting_sub:
                 return False, "Ви вже записані на івент"
-            subscribe = Take_ivent(
+            session.add(Take_event(
                 id_event=event.id,
                 id_user=user.id
-            )
-            session.add(subscribe)
+            ))
             await session.commit()
             return True, "Успішна підписка на івент"
         except SQLAlchemyError as e:
             return f"Проблема в базі данних {e}"
         except Exception as a:
             return f"Виникла помилка {a}"
+
+# Вивід юзерів також дозволено вивід логіну і паролю
+async def user_output():
+    async with async_session() as session:
+        return await session.scalars(select(User))
+
+# Вивід івентів
+async def event_output():
+    async with async_session() as session:
+        return await session.scalars(select(Event))
+
+# Вивід івента для організатора
+async def organizer_event(user_id):
+    async with async_session() as session:
+        return await session.scalars(select(Event).where(Event.organizer==user_id))
+
+# Вивід івентів для юзера де він бере участь
+async def event_for_user(user_id):
+    async with async_session() as session:
+        result = await session.execute(select(Event).join(
+            Take_event, Take_event.id_event==Event.id).where(
+                Take_event.id_user==user_id))
+        return result.scalars().all()
     
-        
-    
+# Вивід учасників для організатора
+async def info_about_user(event_id):
+    async with async_session() as session:
+        result = await session.execute(select(User).join(
+            Take_event, Take_event.id_user==User.id).where(
+            Take_event.id_event==event_id))
+        return result.scalars().all()
+
+# Редагування логіну
+async def edit_login(login):
+    async with async_session() as session:
+        result = await session.scalar(select(User).where(User.login==login))
+        if result:
+            return "Логін вже зайнятий"
+        result.login=login
+        await session.commit()
+
+#Редагування паролю
+async def edit_password(login, password):
+    async with async_session() as session:
+        result = await session.scalar(select(User).where(User.login==login))
+        hashed_pass = bcrypt.hash(password)
+        result.password=hashed_pass
+        await session.commit()
+
+# Редагувати юзера
+async def edit_user(id, first_name, last_name, age, role):
+    async with async_session() as session:
+        user = await session.scalar(select(User).where(User.id==id))
+        user.first_name=first_name
+        user.last_name=last_name
+        user.age=age
+        user.role=role
+        await session.commit()
+   
+# Редагувати евента
+async def edit_event(id,name,time,description,place,location,image_data,role):
+    async with async_session() as session:
+        event = await session.scalar(select(Event).where(Event.id==id))
+        event.name=name
+        event.time=time
+        event.description=description
+        event.place=place
+        event.location=location
+        event.image_data=image_data
+        event.role=role
+        await session.commit()
+   
+# Видалити участь в івенті
+async def delete_activity (id_user, id_event):
+    async with async_session() as session:
+        subscription = await session.scalar(select(Take_event).where(
+            Take_event.id_user==id_user,
+            Take_event.id_event==id_event))
+        if not subscription:
+            return False, "Запис не знайдено"
+        await session.delete(subscription)
+        await session.commit()
+
+# Видалити івент
+async def delete_event(id_event):
+    async with async_session() as session:
+        event = await session.scalar(select(Event).where(Event.id==id_event))
+        if not event:
+            return "Івент не знайдено"
+        await session.delete(event)
+        await session.commit()
+
+# Видалити юзера
+async def delete_user(id_user):
+    async with async_session() as session:
+        user = await session.scalar(select(User).where(User.id==id_user))
+        if not user:
+            return "Юзера не знайдено"
+        await session.delete(user)
+        await session.commit()
